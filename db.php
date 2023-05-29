083785b28003e9a12121d543c6f3c54c237c0ca3
kai <cgk0910@gmail.com>
2023-05-29 14:10:29 +0800

    建立自訂函式find(,)及update(,,)

--- db.php
+++ db.php
@@ -1,7 +1,15 @@
 <?php 
 echo "<pre>";
-print_r(all('options'));
+print_r(find('options',23));
 echo "</pre>";
+/* echo "<pre>";
+print_r(all('options'));
+echo "</pre>"; */
+
+update('options',
+        ['description'=>'50萬','total'=>200],
+        8);
+
 
 function all($table){
     $dsn="mysql:host=localhost;charset=utf8;dbname=vote";
@@ -14,4 +22,44 @@ function all($table){
     return $rows;
 }
 
+function find($table,$id){
+
+    $dsn="mysql:host=localhost;charset=utf8;dbname=vote";
+    $pdo=new PDO($dsn,'root','');
+
+    $sql="select * from `$table`  where `id`='$id' ";
+
+    $row=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+
+    return $row;
+}
+
+//一次更新一筆
+function update($table,$cols,$id){
+    $dsn="mysql:host=localhost;charset=utf8;dbname=vote";
+    $pdo=new PDO($dsn,'root','');
+
+    //['subject'=>'今天天氣很好吧?',
+    // 'open_time'=>'2023-05-29',
+    // 'close_time'=>'2023-06-05',
+    //]
+    $tmp='';
+    foreach($cols as $key => $value){
+
+       $tmp .= "`$key`='$value',";
+
+    }
+
+    //刪除前後的多餘逗號','
+    $tmp=trim($tmp,',');
+
+    //`subject`='今天天氣很好吧?',`open_time`='2023-05-29',`close_time`='2023-06-05'
+
+    $sql="update `$table` set  $tmp where `id`='$id'";
+
+    $result=$pdo->exec($sql);
+
+
+    return $result;
+}
 ?>
