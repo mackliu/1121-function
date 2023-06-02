08a89df233e5fb8670ac36da3dd53634a4c2f756
kai <cgk0910@gmail.com>
2023-06-02 14:01:03 +0800

    物件導向-類別的宣告

---
+++ class.php
@@ -0,0 +1,26 @@
+<?php
+
+class Animal{
+
+    private $age;
+    public $name;
+    protected $hair;
+
+    function __construct()
+    {
+        
+    }
+
+    public function age(){
+
+    }
+
+    protected function run(){
+     
+    }
+
+    private function speed(){
+     
+    }
+
+}
