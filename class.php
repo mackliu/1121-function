7214e904af9556d8336f2d2c1c8d0c2972b36813
kai <cgk0910@gmail.com>
2023-06-02 14:32:18 +0800

    物件的宣告與基本的使用方式

--- class.php
+++ class.php
@@ -2,25 +2,42 @@
 
 class Animal{
 
-    private $age;
-    public $name;
-    protected $hair;
+    private $age=10;
+    protected $name="大雄";
+    protected $hair="紅色 ";
 
     function __construct()
     {
-        
+        echo "建立物件";
     }
-
+    public function getName(){
+        return $this->name;
+    }
+    public function setName($name){
+        return $this->name=$name;
+    }
     public function age(){
-
-    }
+        return $this->age;
+    }   
 
     protected function run(){
-     
+        return "跑跑跑";
     }
 
     private function speed(){
-     
+        return "跑速1000";
     }
 
 }
+
+$animal=new Animal;
+echo "<br>";
+echo $animal->getName();
+$animal->setName("小莉");
+echo $animal->getName();
+//echo $animal->age;
+//echo $animal->hair;
+
+echo $animal->age();
+
+//echo $animal->speed();
